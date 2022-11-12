caf1d53e1f3e4ada8c94ca75d135ded4129f692a
Agus Prasetyo BUdi <agus.prasetyobudi@gmail.com>
2022-11-12 16:43:34 +0700

    update initial start

---
+++ database/migrations/2022_11_12_060356_create_quizzes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('quiz_open');
+            $table->date('quiz_close');
+            $table->time('quiz_limit');
+            $table->string('description');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
